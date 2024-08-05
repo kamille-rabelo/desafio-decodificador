86ecf98da43df3a88ff1aeb1ed61a3a7015bcc0b
kamille-rabelo <kamillersalgado@gmail.com>
2024-08-05 15:02:59 -0300

    modificar funcoes

--- app.js
+++ app.js
@@ -1,83 +1,62 @@
-// const shuffledLetters = [
-//   'q', 'w', 'e', 's', 't', 'y', 'u', 'i', 'z', 'p', 
-//   'a', 'r', 'd', 'f', 'v', 'h', 'm', 'k', 'c', 'o', 
-//   'x', 'l', 'g', 'b', 'n', 'j'
-// ];
-
-// const alphabet = [
-//   'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
-//   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
-//   'u', 'v', 'w', 'x', 'y', 'z'
-// ];
-
-const letters = {
-  'q': 'a', 'w': 'b', 'e': 'c', 's': 'd', 't': 'e', 
-  'y': 'f', 'u': 'g', 'i': 'h', 'z': 'i', 'p': 'j', 
-  'a': 'k', 'r': 'l', 'd': 'm', 'f': 'n', 'v': 'o', 
-  'h': 'p', 'm': 'q', 'k': 'r', 'c': 's', 'o': 't', 
-  'x': 'u', 'l': 'v', 'g': 'w', 'b': 'x', 'n': 'y', 
-  'j': 'z'
-}
-
-function isAlpha(char) {
-  return /^[A-Z]$/i.test(char);
+const letras = {
+  'a': ['ai', 1], 
+  'e': ['enter', 4], 
+  'i': ['imes', 3], 
+  'o': ['ober', 3], 
+  'u': ['ufat', 3], 
 }
 
 function criptografar() {
-  let sentence = ''
+  let sentenca = ''
   const input = document.querySelector('.input-texto').value
+
   for (let index = 0; index < input.length; index++) {
-    let element = input[index];
-    let position;
-    if (isAlpha(element)) {
-      if (element == element.toLowerCase()) {
-        position = parseInt(element.charCodeAt() - 97)
-        element = Object.keys(letters)[position]
-      } else {
-        position = parseInt(element.charCodeAt() - 65)
-        element = Object.keys(letters)[position].toUpperCase()
-      }
+    let elemento = input[index];
+    if (letras.hasOwnProperty(elemento)) {
+      elemento = letras[elemento][0]
     }
-    sentence += element
+    sentenca += elemento
   }
-  preencher_texto_output(sentence)
+
+  preencher_texto_output(sentenca)
 }
 
-function preencher_texto_output(sentence) {
-  const texto = document.querySelector('.output-texto')
-  texto.innerHTML = sentence
-  document.querySelector('.sem-conteudo').style.display = 'none';
-  document.querySelector('.com-conteudo').style.display = 'flex';
+function preencher_texto_output(sentenca) {
+  const inputTexto = document.querySelector('.input-texto').value;
+  const semConteudo = document.querySelector('.sem-conteudo');
+  const comConteudo = document.querySelector('.com-conteudo');
+  const texto = document.querySelector('.output-texto');
+  
+  if (inputTexto != '') {
+      texto.innerHTML = sentenca;
+      semConteudo.style.display = 'none';
+      comConteudo.style.display = 'flex';
+  } else {
+      semConteudo.style.display = 'flex';
+      comConteudo.style.display = 'none';
+  }
 }
 
 function descriptografar() {
-  let sentence = ''
+  let sentenca = ''
   const input = document.querySelector('.input-texto').value
+  
   for (let index = 0; index < input.length; index++) {
-    let element = input[index];
-    let position;
-    if (isAlpha(element)) {
-      if (element == element.toLowerCase()) {
-        element = letters[element]
-      } else {
-        element = letters[element.toLowerCase()].toUpperCase()
-      }
+    let elemento = input[index];
+    if (letras.hasOwnProperty(elemento)) {
+      index += letras[elemento][1] 
     }
-    sentence += element
-    console.log(sentence)
+    sentenca += elemento
   }
-  preencher_texto_output(sentence)
+  
+  preencher_texto_output(sentenca)
 }
 
-// for (let index = 0; index < input.length; index++) {
-//   let element = input[index];
-//   let position;
-//   if (isAlpha(element)) {
-//     if (element == element.toLowerCase()) {
-//       position = parseInt(element.charCodeAt() + 97)
-//     } else {
-//       position = parseInt(element.charCodeAt() + 65)
-//     }
-//     element = position
-//   }
-//   sentence += element
+async function escreverTextoDoPainel() {
+  const texto = document.querySelector('.output-texto').value
+  try {
+    await navigator.clipboard.writeText(texto);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
